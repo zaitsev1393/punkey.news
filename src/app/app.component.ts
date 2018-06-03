99c959cc05181d2713f235674e428321a7b2ae71
zaitsev1393 <zaitsev1393@gmail.com>
2018-06-03 21:41:44 +0300

    ssr4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,17 @@ import {TopicsService} from './services/topics/topics.service';
 export class AppComponent {
   constructor(public topicsService: TopicsService) {
 <<<<<<< HEAD
+<<<<<<< HEAD
     this.topicsService.getTopics().subscribe(topics => {
       console.log(topics);
     })
 =======
 
 >>>>>>> 837a08e... env config added
+=======
+    this.topicsService.getTopics().subscribe(topics => {
+      console.log(topics);
+    })
+>>>>>>> 15ba3c7... topics service set up
   }
 }
