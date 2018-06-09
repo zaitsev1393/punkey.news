50f38b06046d34eeb05f08bf90bdc09d2ec6fd1c
zaitsev1393 <zaitsev1393@gmail.com>
2018-06-09 13:26:08 +0300

    app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {TopicsService} from './services/topics/topics.service';
-import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +6,6 @@ import {ActivatedRoute} from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  public data;
-
-  constructor(public topicsService: TopicsService,
-              private activatedRoute: ActivatedRoute) {
-    // this.topicsService.getTopics().subscribe(topics => {
-    //   console.log(topics);
-    // });
-
+  constructor() {
   }
 }
