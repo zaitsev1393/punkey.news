5229a8b4c80f9f9270b8c5966f8c355e53af33a7
zaitsev1393 <zaitsev1393@gmail.com>
2018-08-04 14:52:04 +0300

    wip

--- src/app/topics/topics.component.ts
+++ src/app/topics/topics.component.ts
@@ -10,9 +10,26 @@ export class TopicsComponent implements OnInit {
 
   public topics;
 
+  public lengthAcc = 0;
+  public lengths = [];
+
   constructor(private activatedRoute: ActivatedRoute) {
     this.topics = this.activatedRoute.snapshot.data['topics'];
     console.log(this.topics);
+  }
+
+  get grid() {
+    let length;
+    if (this.lengthAcc < 2) {
+      length = Math.round(Math.random() * (5 - 3) + 3);
+      this.lengths.push(length);
+      this.lengthAcc++;
+    } else {
+      length = 12 - this.lengths.reduce((a, b) => a + b, 0);
+      this.lengths = [];
+      this.lengthAcc = 0;
+    }
+    return length;
   }
 
   ngOnInit() {
