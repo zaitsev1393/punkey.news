ccfee7c69edd6c9cbba1c0c5537e6b4ead0bc221
zaitsev1393 <zaitsev1393@gmail.com>
2018-08-15 19:42:47 +0300

    env change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBase: 'http://localhost:3000',
+  apiBase: 'https://violet-monkey-api.herokuapp.com',
   token_auth_config: {
     apiBase: 'https://violet-monkey-api.herokuapp.com',
 
