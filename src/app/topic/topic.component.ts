aacbb960a5d300173065817406c1f4195a5a326e
zaitsev1393 <zaitsev1393@gmail.com>
2018-08-28 23:10:13 +0300

    font size for smaller pictures

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TopicComponent implements OnInit {
 
   @Input() topic;
-
+  @Input() size;
   constructor() { }
 
   ngOnInit() {
